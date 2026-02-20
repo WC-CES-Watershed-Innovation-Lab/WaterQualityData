e2ef41e95796f9720b223e7343fc9c00634487f9
Michael Hudak <mhudak3@washcoll.edu>
2026-02-20 08:45:26 -0500

    Comment cleaning & removing extraneous code

--- data_from_hydrovu_v2.py
+++ data_from_hydrovu_v2.py
@@ -15,7 +15,6 @@ import glob
 import sys
 import time
 import base64
-from io import BytesIO, StringIO
 
 start_time = time.time()
 sys.setrecursionlimit(10000) # Increase the limit to 10000
@@ -23,19 +22,6 @@ pio.renderers.default = 'browser' #determines how plot displays
 
 
 # ## Prologue
-
-# In[ ]:
-
-
-
-
-
-
-# In[4]:
-
-
-#%env
-
 
 # In[81]:
 
@@ -59,7 +45,7 @@ LOCAL_DATA_ENDPOINT      = "https://hydrovu.com/public-api/v1/locations/"
 
 # Location IDs dict enables easy transition between site codes and readable site names
 location_ids = {
-    "Mary Bee Gaines Dock AquaTroll" : 4840973161857024,
+    "Lower Langford Creek AquaTroll" : 4840973161857024,
     "Radcliffe Outflow AquaTroll" : 5276098860482560,
     "Millington AquaTroll" : 5687072567394304,
     "SE Creek AquaTroll" : 6000235094540288,
@@ -84,10 +70,6 @@ unit_dict = {row["key_col"]:row["value_col"] for i, row in unit_df.iterrows()}
 
 # In[ ]:
 
-
-
-
-
 # ## SECTION 1
 
 # ## 1. Update Access Token
@@ -111,9 +93,7 @@ def update_access_token():
 
 # In[12]:
 
-
 access_token = update_access_token()
-
 
 # In[13]:
 
@@ -162,13 +142,13 @@ def get_dates(days_ago):
 def make_one_call(desired_location, parameters):
     response = requests.get(url=f"{LOCAL_DATA_ENDPOINT}{location_ids[desired_location]}/data",
                             headers=headers_for_data, params=parameters)
+    
+    # Instead of raising an error, the function returns just a "null" value,
+    # which the dependent functions can handle
     if response.ok:
         return response
     else:
         return "null"
-    #response.raise_for_status()
-
-    #return response
     
     #data contains timestamp/value pairs for each parameterId, and each parameter has a unitId
 
@@ -177,7 +157,6 @@ def make_one_call(desired_location, parameters):
 
 
 def loop_by_date(desired_location, now_date, start_date):
-    #now_date, start_date = get_dates(num_days)
 
     response_list = []
     checked_dates = [] # Anti infinite loop control
@@ -200,9 +179,6 @@ def loop_by_date(desired_location, now_date, start_date):
             break
 
     return response_list
-
-        
-
 
 # In[20]:
 
@@ -241,18 +217,6 @@ def process_responses(response_list):
     return dict_list
 
 
-# In[22]:
-
-
-# this is how combining dictionaries works
-dict1 = {"A" : [2, 4, 6], "B" : [1, 3, 5]}
-dict2 = {"A" : [10, 12], "B" : [11, 13]}
-
-dict3 = {}
-for key in dict1:
-    dict3[key] = dict1[key] + dict2[key]
-
-#print(dict3)
 
 
 # In[23]:
@@ -285,20 +249,6 @@ def merge_dfs(dict_list):
     return list(key_list), combined_dfs
 
 
-fake_list = [1, 1, 3, 4, 5, 6, 7, 7, 8, 9]
-print(set(fake_list))
-
-car = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-
-x = car.keys()
-print(x)
-for key in x:
-    print(key)
-
 # In[24]:
 
 def build_csv(loc, how_many_days_ago):
@@ -314,8 +264,6 @@ def build_csv(loc, how_many_days_ago):
     else:
         print(f"No data in timeframe for {loc}")
         
-#build_csv("Mary Bee Gaines Dock AquaTroll")
-
 
 # In[]:
     
@@ -337,32 +285,7 @@ def update_csv(loc):
     for df in loc_dfs.values():
         df.to_csv(f"{folder_path}\\{df.iloc[0,2]}.csv", mode='a', index=True, header=False)
         
-update_csv("Mary Bee Gaines Dock AquaTroll")
 
-# In[]:
-    
-
-# In[25]:
-
-# for loc in location_ids:
-#     build_csv(loc, 500)
-
-# for loc in location_ids:
-#     update_csv(loc)
-
-
-# This code is great & functional for just getting the previous 2 weeks,
-# but has nothing to do with the csv pathway
-# loc_data_dict = {}
-# for loc in location_ids:
-#     responses = loop_by_date(loc, 14)
-#     response_dict_list = process_responses(responses) # Returns a blank list, [], if the site has no data w/in date range
-#     if response_dict_list == []:
-#         continue
-#     else:
-#         keys, loc_dfs = merge_dfs(response_dict_list)
-#         #
-#         loc_data_dict.update({loc : loc_dfs})
 
 
 # In[26]:
@@ -379,103 +302,9 @@ def dfs_from_csvs(loc):
     except:
         print(f"{loc} csvs do not exist")
 
-
-# In[27]:
-
-
-# The make_one_call function breaks if we don't have recent data for the given location
-#responses = loop_by_date("SE Creek AquaTroll")
-
-
-# In[28]:
-
-
-# for r in responses:
-#     print(r.json()['parameters'][0]['readings'][0])
-#     #print(r.headers['x-isi-next-page'])
-
-
-#  x-isi-next-page: Z4dNGr+OlU+4zl1qrYX3RtxEYfDaH0ENGUQvYmR8Zy+jEWDh4Px7JHCLzLQhLd6X 
-#x-isi-next-page: dxhaIG03uXWEe+mI14zqm/oacUhPQJXEaRsBO189afDxuM2tDjqGMYJ2F0qvclXs 
-#x-isi-next-page: nF09MWMrxbASYxsCvJ6MVGjjwDAyrXtdoWBBAUXnENW2NZ5e31hXzKHAbODN2RiM 
-# 
-# 
-# 
-
-# In[30]:
-
-
-# response_dict_list = process_responses(responses)
-# print(response_dict_list)
-
-
-# In[31]:
-
-
-# keys, all_dfs = merge_dfs(response_dict_list)
-
-
-# In[32]:
-
-
-# print(keys)
-
-
-# In[33]:
-
-
-# # make_graph is defined later
-# for key, value in all_dfs.items():
-#     make_graph(value, value['param_name'][0], value['unit_name'][0]) 
-
-
-# In[34]:
-
-
-# store_api_results is not defined
-# def get_combined_dfs():
-#     global location_ids
-#     for loc in location_ids:
-#         responses = store_api_results("Millington AquaTroll")
-#         response_dict_list = process_responses(responses)
-#         keys, all_dfs = merge_dfs(response_dict_list)
-
-
-# ## ^At this point, I have a dictionary of dataframes for a location
-
-# In[36]:
-
-
-#This code should be returning data (response.json())
-# def get_data(desired_location):
-#     #url is based on the desired location and requires {location_ids[location_name]}
-#     data_page = ''
-#     parameters = {
-#     #"id" : location_ids[desired_location],
-#     "startTime" : 1609477200, # This is the Epoch time for January 1st, 2021
-#     "X-ISI-Start-Page" : data_page
-#     }
-
-    
-#     response = requests.get(url=f"{LOCAL_DATA_ENDPOINT}{location_ids[desired_location]}/data",
-#                             headers=headers_for_data, params=parameters)
-#     response.raise_for_status()
-#     data = response.json()
-#     headers = response.headers
-#     return(data) 
-#     #data contains timestamp/value pairs for each parameterId, and each parameter has a unitId
-
-
-# In[37]:
-
-
-# print(get_data("Millington AquaTroll"))
-
-
 # ## 4. Make the plots (pyplot & plotly)
 
 # In[39]:
-
 
 def make_graph(df, title, unit):
     plt.scatter(df.timestamp, df.value, color='black', marker='o')
@@ -489,11 +318,10 @@ def make_graph(df, title, unit):
 
 # In[40]:
 
-
 def plotly_graph(df, loc, param, unit):
     fig = px.scatter(x=df['timestamp'], y=df["value"],
                  labels={'x': "Time",
-                         'y': f"{loc} {param} {unit}"})
+                         'y': f"{loc} {param} ({unit})"})
 
     #shows interactive plot in browser
     #fig.show()
@@ -503,45 +331,7 @@ def plotly_graph(df, loc, param, unit):
     
 
 
-# In[41]:
-
-
-#iterate through location_ids to grab parameters for each location
-# def main_flow(loc):
-#     loc_data = get_data(loc)
-#     for param in loc_data["parameters"]:
-#         param_name = parameter_dict[param["parameterId"]]
-#         unit_name = unit_dict[param["unitId"]]
-#         loc_df = pd.DataFrame(param["readings"])
-#         #one_plotly(loc_df, f"{loc} {param_name}", unit_name)
-#         print(loc_df.head())
-
-
-# ## 5. Build Graph and .write_html
-
-# In[ ]:
-
-
 #.write_html into a folder for each location, and upload the folder in relevant GitHub page
-
-
-# In[43]:
-
-
-# def download_plotly_html(loc):
-#     loc_data = get_data(loc)
-#     for param in loc_data["parameters"]:
-#         param_name = parameter_dict[param["parameterId"]]
-#         unit_name = unit_dict[param["unitId"]]
-#         loc_df = pd.DataFrame(param["readings"])
-#         plotly_graph(loc_df, loc, param_name, unit_name)
-
-
-# In[45]:
-
-
-#one_plotly("Radcliffe Outflow AquaTroll")
-
 
 # # Main Function Sequence
 
@@ -550,43 +340,6 @@ def plotly_graph(df, loc, param, unit):
 
 #1. Ensure access token is updated and active
 access_token = update_access_token()
-
-#2. Set up dictionary with all available locations
-#locations_dict = get_locations()
-
-#3. Loop through locations_dict and grab data for each location
-# Currently, this loops through each location and writes the html to a folder
-# one html file per parameter and per location
-# for loc in location_ids:
-#     download_plotly_html(loc)
-
-
-# In[50]:
-
-
-#main_flow("SE Creek AquaTroll")
-
-
-# ## View Active Locations
-
-# In[53]:
-
-
-#print(get_locations())
-
-
-# In[ ]:
-
-
-
-
-
-# ## SECTION 2
-
-# In[57]:
-
-
-
 
 
 # In[85]:
@@ -600,24 +353,12 @@ git_headers = {
 }
 
 
-# In[61]:
-
-
-#git_body_params = {
-#    "message" : "Updating the plots",
-#    "content" : content_base64
-#}
-
-
 # In[63]:
 
 
 # Path Parameter constants
 OWNER = "WC-CES-Watershed-Innovation-Lab"
 REPO  = "WaterQualityData"
-
-# PATH needs to be updated for each new graph
-#PATH  = f"docs/{loc}/{wq_param}"
 
 
 # In[65]:
@@ -667,8 +408,6 @@ def convert_dates(time_array):
         local_datetime = datetime.fromtimestamp(val)
         new_dates.append(local_datetime)
     return new_dates
-
-# In[]:
     
 
 # In[71]:
@@ -707,12 +446,13 @@ Must uncomment for loop codes for full functionality.
  - change the number in the buil_csv loop decide how many days in the past to start the csv
  - only run build_csv for a new location, or if an old location's csv gets deleted
  - the update_csv loop must be run before the plotly_bytes loop can have any effect
-"""
+"""    
+
 # for loc in location_ids:
 #     build_csv(loc, 500)
 
-# for loc in location_ids:
-#     update_csv(loc)
+for loc in location_ids:
+    update_csv(loc)
 
 for loc in location_ids:
     dfs_to_convert = dfs_from_csvs(loc) # NoneType if no csv exists
@@ -721,124 +461,8 @@ for loc in location_ids:
             param_name = df.iloc[0,3]
             unit_name = df.iloc[0,4]
             plotly_bytes(df, loc, param_name, unit_name)
-# In[ ]:
 
-
-#########################
-#Run this to print out the html access lines for the index.html file on github
-
-# def print_links(loc):
-#     loc_data = get_data(loc)
-#     for param in loc_data["parameters"]:
-#         param_name = parameter_dict[param["parameterId"]]
-#         encoded_spaces = param_name.replace(' ', '%20')
-#         print(f"<li><a href=\"Millington%20AquaTroll/{encoded_spaces}.html\">{param_name}</a></li>")
-# #print_links("Morgan Creek AquaTroll")
-
-
-# In[ ]:
-
-
-# def print_graph_paths(loc):
-#     loc_data = get_data(loc)
-#     for param in loc_data["parameters"]:
-#         param_name = parameter_dict[param["parameterId"]]
-#         encoded_spaces = param_name.replace(' ', '%20')
-#         encoded_loc = loc.replace(' ', '%20')
-#         print(f"<option value=\"{encoded_loc}/{encoded_spaces}.html\">{param_name}</option>")
-# #print_graph_paths("Radcliffe Outflow AquaTroll")
-
-
-# In[ ]:
-
-
-# def print_graph_paths(loc):
-#     loc_data = get_data(loc)
-#     for param in loc_data["parameters"]:
-#         param_name = parameter_dict[param["parameterId"]]
-#         encoded_spaces = param_name.replace(' ', '%20')
-#         encoded_loc = loc.replace(' ', '%20')
-#         print(f"[\"{param_name}\", \"{encoded_loc}/{encoded_spaces}.html\"],")
-# #print_graph_paths("SE Creek AquaTroll")
-
-
-# # Define run suquence and run_ for each location (updates the plotly graph & data sent to github)
-
-# In[87]:
-
-
-# def run_all(d):
-#     for key,value in dict.items():
-#         for unit_key, df_value in value.items():
-#             param_name = df_value.iloc[0,2]
-#             unit_name = df_value.iloc[0,3]
-#             plotly_bytes(df_value, key, param_name, unit_name)
-
-
-# In[89]:
-
-
-#run_all(loc_data_dict)
-
-
-# In[ ]:
-
-
-#print(loc_data_dict)
-
-
-# In[ ]:
-
-
-# for key, value in loc_data_dict.items():
-#     for unit_key, df_value in value.items():
-#         print(unit_key)
-#     print()
-
-
-# In[ ]:
-
-
-#run_all(loc_data_dict)
-
-
-# In[ ]:
-
-
-# def run_(loc):
-#     loc_data = get_data(loc)
-#     for param in loc_data["parameters"]:
-#         param_name = parameter_dict[param["parameterId"]]
-#         unit_name = unit_dict[param["unitId"]]
-#         loc_df = pd.DataFrame(param["readings"])
-#         plotly_bytes(loc_df, loc, param_name, unit_name)
-
-
-# In[ ]:
-
-
-#run_("Millington AquaTroll")
-
-
-# In[ ]:
-
-
-# for loc in location_ids:
-#     run_(loc)
-
-
-# In[ ]:
-
-
-# url = f"https://api.github.com/repos/{OWNER}/{REPO}/contents/docs/graphs/
-
-
-# In[ ]:
-
-
-# # Make the request
-# response = requests.put(url, headers=headers, json=git_body_params)
-# response.raise_for_status()
-# print(response.json())
 print("--- %s seconds ---" % (time.time() - start_time))
 
+
+#print(convert_dates([1770663600]))
